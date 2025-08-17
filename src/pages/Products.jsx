8ac19d9e9e4a3ad1fd17d19de0ae84651373fbc1
Abdel Kader Diarra <abdelkaderdiarra707@gmail.com>
2025-08-17 19:49:19 +0000

    Filtrons les factures des clients qui sont 0

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -78,7 +78,7 @@ export default function App() {
   };
 
   const backendUrl = import.meta.env.PROD
-    ? 'https://vanchoco-backend-production.up.railway.app'
+    ? 'https://daff-backend-production.up.railway.app'
 
     : 'http://localhost:3001';
 
